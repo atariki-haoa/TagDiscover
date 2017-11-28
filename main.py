e5ba88d11aed49e3d88ef997471ed27592addc8d
arielobos <lobos.ariel@gmail.com>
2017-11-28 18:43:34 -0300

    Agregada funcion
    Detecta tags script que contengan javascript.

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-import sys
+import re
 import requests
+from bs4 import BeautifulSoup
 
-print("Escribe la url a ingresar: "
+print("Escribe la url a ingresar: ")
 url = input()
 req = requests.get(url, stream=True)
 
+html = ''
 for line in req.iter_lines():
-    if line: print(line)
+    if line: 
+        html+=str(line)
+
+soup = BeautifulSoup(html, "html.parser")
+countScriptTags = 0
+jsFindText = soup.findAll('script', type="text/javascript")
+
+for tag in jsFindText:
+    countScriptTags+=1
+
+print("Cantidad de tags javascript no comentados: " + str(countScriptTags))
+
