d6317849b959975e39fc719538a311f5f95af31f
arielobos <lobos.ariel@gmail.com>
2017-11-29 09:37:29 -0300

    Fuera check
    Se elimino check,py porque era innecesario, agregada funcion en el mismo main, obligando a la instalacion de las librerias.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import requests
-from bs4 import BeautifulSoup
-from bs4 import Comment
-
+try:
+    import requests
+    from bs4 import BeautifulSoup
+    from bs4 import Comment
+except ImportError:
+    sys.exit("""Falta instalar algunos paquetes.
+                Instalar mediante pip install -r requirements.txt""")
 
 while(True):
     print("Escribe la url a ingresar: ")
